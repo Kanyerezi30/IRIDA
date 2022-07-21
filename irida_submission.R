4aa301ab61d5563da3d4410653f6701549735f2b
Kanyerezi30 <kanyerezi30@gmail.com>
2022-07-21 14:03:29 +0300

    modified to produce better output

--- irida_submission.R
+++ irida_submission.R
@@ -1,17 +1,30 @@
-# Given a directory path with samples IDs, create a worksheet for IRIDA submission
+# Given a directory path with samples IDs, create a worksheet for IRIDA workbench submission
 
-setwd("/media/kanye/skanyerezi/220716_M05143_0032_000000000-K9VYL/Alignment_1/20220717_153923/Fastq")
-File_Forward <- list.files(pattern = "R1")
-File_Reverse <- list.files(pattern = "R2")
-ids <- list.files(pattern = "*gz")
-ids <- strsplit(ids, '_')
+setwd("path") # set thr working directory to that containing your fastq files
+getwd() # confirm that it is the directory you want 
+File_Forward <- list.files(pattern = "^COV.*R1.*gz") # store the names of the forward reads
+length(File_Forward) # check the number of forward files
 
-Sample_Name <- unique(sapply(ids, "[[", 1))
+File_Reverse <- list.files(pattern = "^COV.*R2,*gz") # store the names of the forward reads
+length(File_Reverse) # check the number of reverse files
 
-Project_ID <- rep(22, length(ids))
-#hedrd <- "[Data]"
-fin <- as.data.frame(cbind(Sample_Name, Project_ID, File_Forward, File_Reverse))
-View(fin)
-# create the worksheet
-library(writexl)
-write_xlsx(fin, path = "./fin.xlsx")
+length(File_Forward) == length(File_Reverse) # check whether the number of forward is equal to that of the reverse
+
+Sample_Name <- sapply(strsplit(File_Forward, "_"), "[[", 1) # get the sample names
+length(Sample_Name) # check the number of samples
+
+length(File_Forward) == length(Sample_Name) # confirm that the number of forward reads is equal to the number of samples
+
+Project_ID <- rep(22, length(Sample_Name)) # create the project id given by the workbench
+
+fin <- as.data.frame(cbind(Sample_Name, Project_ID, File_Forward, File_Reverse)) # create final output dataframe
+
+# create the first row needed
+header <- c("[Data]", "NA", "NA", "NA")
+
+# create the second row
+sheader <- c("Sample_Name", "Project_ID", "File_Forward", "File_Reverse")
+
+fin <- rbind(header, sheader, fin) # add the two first rows to the final desired output
+
+xlsx::write.xlsx(fin, "SampleList.xlsx", col.names = F, append = F) # create the final output
